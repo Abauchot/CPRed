bd9b4205ba727ad869548e88d17916c30ae16145
Antoine Perso <kromgrim21@gmail.com>
2023-08-11 15:54:05 +0200

    adding pic roles mini

--- src/app/roles/page.js
+++ src/app/roles/page.js
@@ -2,26 +2,26 @@ import React from "react";
 import Navbar from "@/app/components/navbar";
 import RolesDisplay from "@/app/roles/rolesDisplay";
 
-
 export default function Roles() {
     return (
         <div>
-            <Navbar/>
-            <div className={"justify-center"}>
-                <h2>Le coeur du jeu de rôle Cyberpunk</h2>
-                <p>
-                    Malgré une guerre généralisée entre des mégacorporation suivi par une explosion atomique,
-                    l'univers de Cyberpunk combine la sauvagerie, la sophistication, la modernité et un aspect rétrograde.
-                    Des Techies flamboyants aux allures de top models côtoient des guerriers de la route prêts au combat, et tout ce beau monde
-                    apparaît dans les boîtes de nuit les plus torrides, le bars sordides et les rues barbares de cette ville héritière de l'holocauste.
-                    Chacun d'entre eux remplit un <strong>rôle</strong>: l'occupation principale qu'on leur connait dans la Rue.<br/>
-                   <strong>Dans le futur de Cyberpunk RED, il existe dix rôles : </strong>
-                </p>
-
+            <Navbar />
+            <div className="flex justify-center items-center">
+                <div className="text-center">
+                    <h1 className='text-4xl font-normal leading-normal mt-2 mb-2 text-black-800'>Le coeur du jeu de rôle Cyberpunk</h1>
+                    <p>
+                        Malgré une guerre généralisée entre des mégacorporation suivi par une explosion atomique,
+                        l&apos;univers de Cyberpunk combine la sauvagerie, la sophistication, la modernité et un aspect rétrograde.
+                        Des Techies flamboyants aux allures de top models côtoient des guerriers de la route prêts au combat, et tout ce beau monde
+                        apparaît dans les boîtes de nuit les plus torrides, le bars sordides et les rues barbares de cette ville héritière de l&apos;holocauste.
+                        Chacun d&apos;entre eux remplit un <strong>rôle</strong> l&apos;occupation principale qu&apos;on leur connait dans la Rue.<br />
+                        <strong>Dans le futur de Cyberpunk RED, il existe dix rôles: </strong>
+                    </p>
+                </div>
             </div>
             <div>
-                <RolesDisplay/>
+                <RolesDisplay />
             </div>
         </div>
-    )
+    );
 }
--- src/app/roles/rolesDisplay.js
+++ src/app/roles/rolesDisplay.js
@@ -7,73 +7,78 @@ export default function RolesDisplay() {
             name: 'Rockeurs',
             shortDescription: "Rellebes rock'n'roll qui défient l'autorité à l'aide de conerts, d'oeuvre d'art et discours enflammés",
             imageUrl:
-                '',
+                'images/tokens_roles/rockeur_token.png',
         },
         {
             name: 'Solos',
             shortDescription: "Assassins, gardes du corps, tueurs à gages et soldats à louer dans un nouveau où la loi n'a plus cours",
             imageUrl:
-                '',
+                'images/tokens_roles/solo_token.png',
         },
         {
             name: 'Netrunners',
             shortDescription: "Maîtres cybernitiques du pritage de l'après-NET et cambrioleurs des secrets de la psyché.",
             imageUrl:
-                '',
+                'images/tokens_roles/netrunner_token.png',
         },
         {
             name: 'Techies',
             shortDescription: "Mécaniciens renégats et inventeurs des supertechnologies; ce sont eux qui assurent le fonctionnement de ce futur sombre.",
             imageUrl:
-                '',
+                'images/tokens_roles/techie_token.png',
         },
         {
             name: 'Medtechs',
             shortDescription: "Docteurs de quartiers qui excerent sans autorisation et médecins de la cybernétique; ils répartent aussi bien la chair que le métal.",
             imageUrl:
-                '',
+                'images/tokens_roles/medtech_token.png',
         },
         {
             name: 'Médias',
             shortDescription: "Journalistes, stars médiatiques et influenceurs qui risquent tout au nom de la vérité - ou pour la gloire.",
             imageUrl:
-                '',
+                'images/tokens_roles/media_token.png',
         },
         {
             name: 'Corporatistes',
             shortDescription: "Cadres d'entreprise et corsaises du monde du business qui luttent pour rétable le règle des mégacorporations.",
             imageUrl:
-                '',
+                'images/tokens_roles/corporatiste_token.png',
         },
         {
             name: 'Justiciers',
             shortDescription: "Des agents de l'autorité augmentés qui patrouillent dans les rue sauvages et les autoroutes où règent les barbares de la route.",
             imageUrl:
-                '',
+                'images/tokens_roles/justicier_token.png',
         },
         {
             name: 'Fixers',
             shortDescription: "Trafiquants, contrebandiers, négociateurs et informateurs qui règnent sur le marchés de minuit, dans la rue d'après-guerre.",
             imageUrl:
-                '',
+                'images/tokens_roles/fixer_token.png',
         },
         {
             name: 'Nomades',
             shortDescription: "Experts en transports, quintessence des guerriers de la route, pirates et contrebandiers qui relient les communautés du monde entier.",
             imageUrl:
-                '',
+                'images/tokens_roles/nomade_token.png',
         },
     ]
 
     return (
-        <div className="grid gap-4 grid-cols-3 grid-rows-3 mt-2.5">
-            {roles.map((roles) =>(
-                <div key={roles.name} className='flex items-center gap-x-6'>
-                    <img className='h-16 w-16 rounded-full' src={roles.imageUrl} alt={roles.name}/>
-                    <h3 className='text-base font-semibold leading-7 tracking-tight text-gray-900'>{roles.name}</h3>
-                    <p>{roles.shortDescription}</p>
-                </div>
-            ))}
+        <div className="flex justify-center items-center">
+            <div className="container grid gap-12 grid-cols-2 grid-rows-2 mt-2.5">
+                {roles.map((role) => (
+                    <div key={role.name} className="flex flex-col items-center gap-x-6 mt-2.5 mb-2.5">
+                        <img className="h-16 w-16 rounded-full" src={role.imageUrl} alt={role.name} />
+                        <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{role.name}</h3>
+                        <p className="break-normal">{role.shortDescription}</p>
+                        <div>
+                            <button className='border-2'>afficher</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
